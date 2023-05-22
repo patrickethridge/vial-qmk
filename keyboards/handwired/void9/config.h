4bcddd41d7c9b5918ebe6d130a0a6b8152e05e31
patrickethridge <132078953+patrickethridge@users.noreply.github.com>
2023-05-22 07:15:49 -0500

    Added Void9
    Added Void9.  Tested working.

---
+++ keyboards/handwired/void9/config.h
@@ -0,0 +1,24 @@
+// Copyright 2023 patrickethridge (@patrickethridge)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+#define DEBOUNCE 5
+
+#define ENCODERS_PAD_A { B1, B2 }
+#define ENCODERS_PAD_B { F7, B3 }
